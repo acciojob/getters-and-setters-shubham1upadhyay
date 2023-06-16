7ed5da25e7d36c737cfc192d098f84e824240c8d
Shubham Upadhyay <shubhamup47@gmail.com>
2023-06-16 17:58:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+	getName()
+	{
+		return this.name;
+	}
+	getAge()
+	{
+		return this.age;
+	}
+	setName(newName)
+	{
+		this.name = newName;
+	}
+	setAge(newAge)
+	{
+		this.age = newAge;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study()
+	{
+		console.log(`${this.name} "is studying"`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach()
+	{
+		console.log(`${this.name} "is teaching"`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
